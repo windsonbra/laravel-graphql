d8415532b5c9164c7d5885ef447f70e61f4fccda
Windson Viana <windson.viana@tjdft.jus.br>
2020-09-25 20:46:06 -0300

    primeiro como commit

---
+++ app/Email.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Email extends Model
+{
+    protected $table = 'email';
+    protected $fillable = [
+        'title', 'body'
+    ];
+}
